3bf0ce745e044e14b1ef2441cfe1abad5d3f5a11
aminamarat1101 <aminamaratt@gmail.com>
2025-01-25 03:27:17 +0000

    files added

---
+++ class6-module/main.tf
@@ -0,0 +1,8 @@
+module "november-2024" {
+  source  = "aminamarat1101/november-2024/module"
+  version = "2.0.0"
+  region = "us-east-1"
+  vpc_cidr = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
+}
+
